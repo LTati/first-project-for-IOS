bf703e2337e640c80d25bdff8baae71e4becc163
Ltati <proksi_n@mail.ru>
2022-11-13 00:41:29 +0300

    first commit from MacOS for IOS

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by user on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
